9bb5eb2a8d30d4b0767fe2d5e9d358d3adfdd852
Ran <lan4250@naver.com>
2022-08-08 19:45:59 +0900

    Initial commit

---
+++ src/main/java/com/example/javastudy_02/HelloJava.java
@@ -0,0 +1,12 @@
+package com.example.javastudy_02;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hello");
+
+    }
+
+}
---
+++ src/main/java/com/example/javastudy_02/JavaStudy02Application.java
@@ -0,0 +1,13 @@
+package com.example.javastudy_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudy02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStudy02Application.class, args);
+    }
+
+}
