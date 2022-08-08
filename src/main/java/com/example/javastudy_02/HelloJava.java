bcedb334d484663761b09460e7d7480e498abb32
Ran <lan4250@naver.com>
2022-08-08 19:56:28 +0900

    hello hello hello hello

--- src/main/java/com/example/javastudy_02/HelloJava.java
+++ src/main/java/com/example/javastudy_02/HelloJava.java
@@ -2,5 +2,9 @@ package com.example.javastudy_02;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
     }
 }
